bcd1c19b28f87a41c676e36352a9c921d9d698bc
Austin Albrecht <austin.aa@me.com>
2020-04-19 17:52:28 +0200

    Complete P06 & fix more retarded makefiles

--- P06_Integer_Rechner/integer-rechner/src/stack.c
+++ P06_Integer_Rechner/integer-rechner/src/stack.c
@@ -30,23 +30,30 @@ stack_t *stack_new(size_t max_elements)
 {
     stack_t *instance = NULL;
     // 1. allocate a stack_t instance on the heap and set the instance variable to it
+    instance = malloc(sizeof(stack_t));
+
     // 2. call error_fatal_errno("stack_new: instance"); if failed to allocate the memory on the heap
+    if (!instance)
+    {
+        error_fatal_errno("stack_new: instance");
+    }
+    
     // 3. allocate an array of max_elements value_t's on the heap and store its address in the stack member of the stack_t instance
+    instance->stack = malloc(max_elements * sizeof(stack_value_t)); 
+    
     // 4. call error_fatal_errno("stack_new: stack"); if failed to allocate the memory on the heap
-    // 5. set the top member of the stack_t instance to the address of the "element" before the first stack array element
+    if (!instance->stack)
+    {
+        error_fatal_errno("stack_new: stack");
+    }
+    
+    // 5. set the top member of the stack_t instance to the 
+    //    address of the "element" before the first stack array element
+    instance->top = instance->stack - 1;
+
     // 6. set the full member of the stack_t instance to the address of the last element of the stack array
-    // BEGIN-STUDENTS-TO-ADD-CODE
+    instance->full = instance->top + max_elements;
 
-
-
-
-
-
-
-
-
-
-    // END-STUDENTS-TO-ADD-CODE
     return instance;
 }
 
@@ -54,13 +61,11 @@ void stack_destroy(stack_t *instance)
 {
     assert(instance);
     // 1. free the stack array
+    free(instance->stack);
+
     // 2. free the stack_t instance
-    // BEGIN-STUDENTS-TO-ADD-CODE
+    free(instance);
 
-
-
-
-    // END-STUDENTS-TO-ADD-CODE
 }
 
 void stack_push(stack_t *instance, stack_value_t value)
@@ -68,12 +73,11 @@ void stack_push(stack_t *instance, stack_value_t value)
     assert(instance);
     if (stack_is_full(instance)) error_fatal("stack_push: full");
     // 1. increment by one element the address stored in the top member of the stack_t instance
-    // 2. store the received value at the new top location
-    // BEGIN-STUDENTS-TO-ADD-CODE
+	instance->top++;
 
+	// 2. store the received value at the new top location
+    *instance->top = value;
 
-
-    // END-STUDENTS-TO-ADD-CODE
 }
 
 stack_value_t stack_pop(stack_t *instance)
@@ -82,12 +86,11 @@ stack_value_t stack_pop(stack_t *instance)
     if (stack_is_empty(instance)) error_fatal("stack_pop: empty");
     stack_value_t value = 0;
     // 1. set the variable value to the value from the address the top member points to
+    value = *(instance->top);
+
     // 2. decrement by one element the address stored in the top member of the stack_t instance
-    // BEGIN-STUDENTS-TO-ADD-CODE
+    instance->top--;
 
-
-
-    // END-STUDENTS-TO-ADD-CODE
     return value;
 }
 
@@ -97,36 +100,24 @@ stack_value_t stack_top(stack_t *instance)
     if (stack_is_empty(instance)) error_fatal("stack_top: empty");
     stack_value_t value = 0;
     // 1. set the variable value to the value from the address the top member points to
-    // BEGIN-STUDENTS-TO-ADD-CODE
-
-
+    value = *(instance->top);
     
-    // END-STUDENTS-TO-ADD-CODE
     return value;
 }
 
 int stack_is_empty(stack_t *instance)
 {
     assert(instance);
-    int is_empty = 1;
+    // int is_empty = 1;
     // 1. set is_empty to 1 if the top equals the initial condition as done in stack_new(), otherwise to 0
-    // BEGIN-STUDENTS-TO-ADD-CODE
-
-
-    
-    // END-STUDENTS-TO-ADD-CODE
-    return is_empty;
+    return instance->top == instance->stack - 1;
 }
 
 int stack_is_full(stack_t *instance)
 {
     assert(instance);
-    int is_full = 1;
+    // int is_full = 1;
     // 1. set is_full to 1 if the top equals the full pointer as set in the stack_new() function, otherwise 0
-    // BEGIN-STUDENTS-TO-ADD-CODE
 
-
-    
-    // END-STUDENTS-TO-ADD-CODE
-    return is_full;
+    return instance->top == instance->full;
 }
